bbc4ef55256fe69c26914ce4690e776d0f4db8ed
Peter Jin <peterhaijin@gmail.com>
2017-01-28 11:27:53 -0800

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// These are "icc" or "icpc" redistributable libraries.
+
+#[link(name = "irc")]
+extern "C" {}
+
+#[link(name = "svml")]
+extern "C" {}
+
+#[link(name = "imf")]
+extern "C" {}
+
+// TODO
